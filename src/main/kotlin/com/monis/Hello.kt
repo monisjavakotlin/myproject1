f3618541192068eae4929174a94952790906dc44
monis <monis377272@gmail.com>
2018-12-18 15:04:45 +0800

    Java and kotlin variable, data type

--- src/main/kotlin/com/monis/Hello.kt
+++ src/main/kotlin/com/monis/Hello.kt
@@ -5,9 +5,14 @@ fun main(args: Array<String>) {
 //    Human().hello()
     val h = Human()
     h.hello()
+    val age : Int = 19
+    var weight = 66.5
+    var height = 1.7f
+    var name : String
+    name = "monis"
 }
 
-class Human {
+class Human{
     fun hello() {
         println("Hello Kotlin")
     }
